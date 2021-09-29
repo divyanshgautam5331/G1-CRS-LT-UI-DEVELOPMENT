5e6700216e8b1ec8a030cd154fdb74cbb4b0a6a3
divyanshgautam5331 <gautamdivyansh5331@gmail.com>
2021-09-29 17:38:42 +0530

    UI Project Structure Created

---
+++ src/app/Component/student/registercourse/registercourse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registercourse',
+  templateUrl: './registercourse.component.html',
+  styleUrls: ['./registercourse.component.css']
+})
+export class RegistercourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './Component/student/student.component';
+import { ProfessorComponent } from './Component/professor/professor.component';
+import { AdminComponent } from './Component/admin/admin.component';
+import { UserComponent } from './Component/user/user.component';
+import { RegistercourseComponent } from './Component/student/registercourse/registercourse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    ProfessorComponent,
+    AdminComponent,
+    UserComponent,
+    RegistercourseComponent
   ],
   imports: [
     BrowserModule,
